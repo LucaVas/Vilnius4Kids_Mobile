6fd0df99dce123592a0d60f1bb53fc431467c29e
Luca Vassos <104051992+LucaVas@users.noreply.github.com>
2025-01-15 16:40:36 +0200

    Add 'more' screen (#7)

---
+++ client/app/(tabs)/more.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet, Text, View} from "react-native";
+
+export default function MoreScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>More screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {}
+})
---
+++ client/components/tab/TabIcon.tsx
@@ -0,0 +1,14 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+type AvailableIconNames = 'map-sharp' | 'map-outline' | 'heart-sharp' | 'heart-outline' | 'menu-sharp' | 'menu-outline';
+
+type Props = {
+    color: string,
+    name: AvailableIconNames,
+}
+
+export default function TabIcon({color, name}: Props) {
+    return (
+        <Ionicons name={name} color={color} size={24}/>
+    );
+}
