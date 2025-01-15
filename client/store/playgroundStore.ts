0fbf13ecc9ff2209ab79facf818e94ecdbb3d37d
LucaVas <luca.vassos@gmail.com>
2025-01-15 17:06:41 +0200

    Add playgrounds mock data

--- client/store/playgroundStore.ts
+++ client/store/playgroundStore.ts
@@ -1,11 +1,31 @@
 import {create} from 'zustand'
 
-type Playground = {}
+const mockData: Playground[] = [
+    {
+        coordinates: {
+            latitude: 54.683835,
+            longitude: 25.279269,
+        },
+        status: 'open'
+    },
+    {
+        coordinates: {
+            latitude: 54.683860,
+            longitude: 25.279222,
+        },
+        status: 'closed'
+    }
+]
+
+type Playground = {
+    coordinates: { latitude: number, longitude: number },
+    status: 'open' | 'closed'
+}
 
 interface PlaygroundState {
     playgrounds: Playground[]
 }
 
 export const usePlaygroundStore = create<PlaygroundState>()((set) => ({
-    playgrounds: []
+    playgrounds: mockData
 }))
